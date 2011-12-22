04769bb47fa30d673fcc25a698c787c9a1b87973
Zero86 <skandich@yandex.ru>
2011-12-22 02:34:30 +0200

    update project

--- my42cc/settings.py
+++ my42cc/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqllite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'test_db.sqlite3',                      # Or path to database file if using sqlite3.
+        'NAME': '42ccdb.sqlite3',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
